15c0468a53f9cea72e827d5ea55ad64e98511d77
Jk Kuei <momonbin1991@gmail.com>
2022-04-03 04:34:55 +0800

    Day 8 Form wave Finished

---
+++ Day8_Form_wave/app.js
@@ -0,0 +1,11 @@
+const labels = document.querySelectorAll(".form-control label");
+
+labels.forEach(label => {
+    label.innerHTML = label.innerText
+        .split('')
+        .map((latter, idx) => {
+            return `<span style= transition-delay:${idx * 50}ms>${latter}</span>`
+        })
+        .join("");
+})
+
